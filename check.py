2b1c02b6983b77ad10f29ab1ca24770e8de408f5
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-07-11 16:29:39 +0900

    preprocessing
    1. change NA to 0
    2. Get month & add Month column
    3. check holiday & add NumHoliday column

--- check.py
+++ check.py
@@ -4,3 +4,23 @@ train=pd.read_csv('dataset/train.csv')
 test=pd.read_csv('dataset/test.csv')
 sample_submission=pd.read_csv('dataset/sample_submission.csv')
 
+#change NA
+train=train.fillna(0)
+
+#add month column
+def get_month(date):
+    month=date[3:5]
+    month=int(month)
+    return month
+
+train['Month']=train['Date'].apply(get_month)
+
+#check holiday
+def holiday_to_num(isholiday):
+    if isholiday==True:
+        num=1
+    else:
+        num=0
+    return num
+
+train['NumHoliday']=train['IsHoliday'].apply(holiday_to_num)
